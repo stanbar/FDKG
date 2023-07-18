574b08219bf58940ce954597a5eaa4e1dc316ec8
Stanislaw Baranski <stan@stan.bar>
2023-07-18 18:31:32 +0200

    add elGamal encryption

---
+++ app/elGamal.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/torusresearch/pvss/common"
+	"github.com/torusresearch/pvss/pvss"
+	"github.com/torusresearch/pvss/secp256k1"
+)
+
+// TODO: make it deterministic and independent of the base point
+var H = common.BigIntToPoint(secp256k1.Curve.ScalarBaseMult(pvss.RandomBigInt().Bytes()))
+
+func EncryptBoolean(yesOrNo bool, votingPublicKey *common.Point, voter PublicKey) EncryptedBallot {
+	blindingFactor := pvss.RandomBigInt()
+	comm := common.BigIntToPoint(secp256k1.Curve.ScalarBaseMult(blindingFactor.Bytes()))
+
+	// k_i * E
+	X, Y := secp256k1.Curve.ScalarMult(&votingPublicKey.X, &votingPublicKey.Y, blindingFactor.Bytes())
+
+	if yesOrNo {
+		return EncryptedBallot{voter: voter, a: comm, b: common.BigIntToPoint(secp256k1.Curve.Add(X, Y, &H.X, &H.Y))}
+	} else {
+		return EncryptedBallot{voter: voter, a: comm, b: common.BigIntToPoint(X, Y)}
+	}
+}
