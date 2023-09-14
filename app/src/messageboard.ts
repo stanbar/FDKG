9058815fcdd19059c25caa10cfd4c7788cb6a496
Stanislaw Baranski <stan@stan.bar>
2023-09-14 20:26:56 +0200

    fix aggregation from zero to a first element

--- app/src/messageboard.ts
+++ app/src/messageboard.ts
@@ -16,39 +16,44 @@ export function MessageBoard(config: VotingConfig) {
     const partialDecryptions: Array<BabyJubPoint> = []
 
     const votingPubKey = () => {
-        return votingPublicKeys.reduce((acc, key) => {
+        return votingPublicKeys.slice(1).reduce((acc, key) => {
             return addPoint(acc, key)
-        }, [F.e("0"), F.e("0")])
+        }, votingPublicKeys[0])
     }
 
-    const contributeDkg = async (node: LocalParty, proof: Proof, publicSignals: PublicSignals, shares: EncryptedShare[], votingPublicKey: PubKey) => {
+    const contributeDkg = async (node: PublicParty, proof: Proof, publicSignals: PublicSignals, shares: EncryptedShare[], votingPublicKey: PubKey) => {
         const valid = await verifyPVSS(proof, publicSignals)
         if (!valid) {
             throw new Error("Invalid proof")
         }
+        console.log(`contributed dkg from node ${node.index}`)
         votingPublicKeys.push(votingPublicKey)
-        sharesFrom.set(node.publicParty.publicKey, shares)
+        sharesFrom.set(node.publicKey, shares)
     }
 
-    const publishVote = async (encryptedBallot: { C1: BabyJubPoint, C2: BabyJubPoint, proof: Proof, publicSignals: PublicSignals }) => {
+    const publishVote = async (node: PublicParty, encryptedBallot: { C1: BabyJubPoint, C2: BabyJubPoint, proof: Proof, publicSignals: PublicSignals }) => {
         const valid = await verifyBallot(encryptedBallot.proof, encryptedBallot.publicSignals)
         if (!valid) {
             throw new Error("Invalid proof")
         }
+        console.log(`published vote from node ${node.index}`)
         votes.push([encryptedBallot.C1, encryptedBallot.C2])
     }
 
     const aggregatedBallots = async (): Promise<BabyJubPoint> => {
-        return votes.map(vote => vote[0]).reduce((acc, vote) => {
-            return addPoint(acc, vote)
-        }, [F.e("0"), F.e("0")])
+        return votes.slice(1).map(vote => vote[0]).reduce((acc, vote) => {
+            const sum = addPoint(acc, vote)
+            console.log({ acc, vote, sum })
+            return sum
+        }, votes[0][0])
     }
 
-    const publishPartialDecryption = async (partialDecryption: {
+    const publishPartialDecryption = async (node: PublicParty, partialDecryption: {
         partialDecryption: BabyJubPoint,
         proof: Proof,
         publicSignals: PublicSignals,
     }[]) => {
+        console.log(`published partial decryption from node ${node.index}`)
         partialDecryption.forEach(async (partialDecryption) => {
             const valid = await verifyBallot(partialDecryption.proof, partialDecryption.publicSignals)
             if (!valid) {
@@ -64,7 +69,7 @@ export function MessageBoard(config: VotingConfig) {
         for (let [from, tos] of sharesFrom) {
             const index = tos.findIndex(to => to.guardianPubKey === node.publicKey)
             if (index != -1) {
-                sharesForNode.push({ index, sharesSize: tos.length, ...tos[index] })
+                sharesForNode.push({ index : index + 1, sharesSize: tos.length, ...tos[index] })
             }
         }
         return sharesForNode
@@ -72,14 +77,14 @@ export function MessageBoard(config: VotingConfig) {
 
     const offlineTally = () => {
         // TODO: test if it coffect
-        const Z = partialDecryptions.reduce((acc, partialDecryption) => {
+        const Z = partialDecryptions.slice(1).reduce((acc, partialDecryption) => {
             return addPoint(acc, partialDecryption)
-        }, [F.e("0"), F.e("0")])
+        }, partialDecryptions[0])
 
         // TODO: test if it coffect
-        const C2 = votes.map(v => v[1]).reduce((acc, vote) => {
+        const C2 = votes.slice(1).map(v => v[1]).reduce((acc, vote) => {
             return addPoint(acc, vote)
-        }, [F.e("0"), F.e("0")])
+        }, votes[0][1])
 
         const [x_1, x_2, x_3, x_4] = decryptResults(Z, C2, config.size, config.options)
         return [x_1, x_2, x_3, x_4]
