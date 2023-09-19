ab5a7222199a0217cd340d40b31196f643ee7bc6
Stanislaw Baranski <stan@stan.bar>
2023-09-19 17:24:13 +0200

    add encryption messages

--- shared-crypto/src/ballot.ts
+++ shared-crypto/src/ballot.ts
@@ -1,6 +1,7 @@
 import assert from 'node:assert';
-import { BabyJubPoint, Base8, SNARK_FIELD_SIZE, addPoint, mulPointEscalar } from './babyjub';
+import { Base8, SNARK_FIELD_SIZE, addPoint, mulPointEscalar } from './babyjub';
 import * as F from './F';
+import { BabyJubPoint } from './types';
 
 /*
     We use ballot encoding as defined by Baudron et al. Practical multi-candidate election system
@@ -31,16 +32,41 @@ const encodeBallotCast = (votingPubKey: BabyJubPoint, cast: bigint, r: bigint, v
     const c2 = addPoint(mG, rP)
     return c2
 }
-
 export const encryptBallot = (votingPubKey: BabyJubPoint, cast: bigint, r: bigint, voters: number, options: number): [BabyJubPoint, BabyJubPoint] => {
     const C1: BabyJubPoint = mulPointEscalar(Base8, r)
     const C2: BabyJubPoint = encodeBallotCast(votingPubKey, cast, r, voters, options)
     return [C1, C2]
 }
 
+export const encryptMessage = (votingPubKey: BabyJubPoint, cast: bigint, r: bigint, voters: number, options: number): [BabyJubPoint, BabyJubPoint] => {
+    const C1: BabyJubPoint = mulPointEscalar(Base8, r)
+    const mG = mulPointEscalar(Base8, cast)
+    const rP = mulPointEscalar(votingPubKey, r)
+    const C2: BabyJubPoint = addPoint(mG, rP)
+    return [C1, C2]
+}
+
 export const decryptBallot = (c1: BabyJubPoint, c2: BabyJubPoint, privKey: bigint, voters: number, options: number): bigint => {
     const mBits = findMbits(voters)
     const c1r = mulPointEscalar(c1, privKey)
+    const c1rXNeg = F.neg(c1r[0])
+    const mG = addPoint(c2, [c1rXNeg, c1r[1]])
+    let x = 1n
+    while (x <= options) {
+        const exponent = ((x - 1n) * mBits)
+        const message = (2n ** exponent) % SNARK_FIELD_SIZE
+
+        const decoded = mulPointEscalar(Base8, message)
+        if (F.toBigint(decoded[0]) === F.toBigint(mG[0]) && F.toBigint(decoded[1]) === F.toBigint(mG[1])) {
+            return x
+        }
+        x += 1n
+    }
+    throw Error("Could not decrypt ballot")
+}
+
+export const decryptBallotMpc = (c1r: BabyJubPoint, c2: BabyJubPoint, voters: number, options: number): bigint => {
+    const mBits = findMbits(voters)
     const c1rXNeg = F.neg(c1r[0])
     const mG = addPoint(c2, [c1rXNeg, c1r[1]])
     let x = 1n
@@ -66,6 +92,7 @@ export const decryptResults = (c1r: BabyJubPoint, c2: BabyJubPoint, voters: numb
 const exhoustiveSearch = (M: BabyJubPoint, voters: number, options: number): [bigint, bigint, bigint, bigint] => {
     const mBits = findMbits(voters)
     let rounds = 0n
+    const votersBigint = BigInt(voters)
     // x_1 * 2^((option_1-1) * mBits)
     const exponentForOptionOne = ((1n - 1n) * mBits)
     const optionOne = (2n ** exponentForOptionOne) % SNARK_FIELD_SIZE
@@ -79,9 +106,9 @@ const exhoustiveSearch = (M: BabyJubPoint, voters: number, options: number): [bi
     const exponentForOptionFour = ((4n - 1n) * mBits)
     const optionFour = (2n ** exponentForOptionFour) % SNARK_FIELD_SIZE
     for (let x_1 = 0n; x_1 <= voters; x_1++) {
-        for (let x_2 = 0n; x_2 <= BigInt(voters) - x_1; x_2++) {
-            for (let x_3 = 0n; x_3 <= BigInt(voters) - x_1 - x_2; x_3++) {
-                for (let x_4 = 0n; x_4 <= BigInt(voters) - x_1 - x_2 - x_3; x_4++) {
+        for (let x_2 = 0n; x_2 <= votersBigint - x_1; x_2++) {
+            for (let x_3 = 0n; x_3 <= votersBigint - x_1 - x_2; x_3++) {
+                for (let x_4 = 0n; x_4 <= votersBigint - x_1 - x_2 - x_3; x_4++) {
                     const sum = x_1 * optionOne + x_2 * optionTwo + x_3 * optionThree + x_4 * optionFour
                     rounds += 1n
                     const decoded = mulPointEscalar(Base8, sum)
