517f8d4e7343859e58017c6df33d5c54a0faa5a2
Stanislaw Baranski <stan@stan.bar>
2023-09-22 19:34:24 +0200

    move lagrange coef multiplication to offline stage

--- app/tests/fdkg.test.ts
+++ app/tests/fdkg.test.ts
@@ -1,10 +1,11 @@
 import assert from 'assert';
-import { BabyJubPoint, PubKey, inCurve, addPoint, decryptResults, encryptShare, genKeypair, genPubKey, genRandomSalt, sss } from "shared-crypto";
+import { BabyJubPoint, PubKey, inCurve, addPoint, decryptResults, mulPointEscalar } from "shared-crypto";
 import { VotingConfig } from '../src/messageboard';
-import { EncryptedShare, LocalParty } from '../src/party';
-import { randomPolynomialZ, evalPolynomialZ } from 'shared-crypto/src/sss';
+import { EncryptedShare } from '../src/party';
 import { PointZero } from 'shared-crypto/src/F';
 import { generateSetOfNodes } from '../src/utils';
+import { LagrangeCoefficient, recoverZ } from 'shared-crypto/src/sss';
+import _ from 'lodash';
 
 describe("fdkg", () => {
     it("should perform fdkg without proofs", () => {
@@ -23,11 +24,12 @@ describe("fdkg", () => {
             [8, [8, 5, 6, 7]],
             [10, [10, 7, 8, 9]]
         ]
+        const talliers = [3, 6, 7, 8]
         const config: VotingConfig = {
             size: nodeIndicies.length,
             options: 4,
             guardiansSize: 4,
-            guardiansThreshold: 3,
+            guardiansThreshold: 2,
             skipProofs: true,
         }
 
@@ -40,13 +42,15 @@ describe("fdkg", () => {
         const votingPublicKeys: BabyJubPoint[] = []
         const votes: { C1: BabyJubPoint, C2: BabyJubPoint }[] = []
         const sharesFrom: Map<PubKey, Array<EncryptedShare>> = new Map()
+        const partialDecryptionFor: Map<PubKey, Array<{ senderPubKey: PubKey, pd: BabyJubPoint }>> = new Map()
         const partialDecryptions: Array<BabyJubPoint> = []
 
         guardianSets.forEach(([nodeIndex, guardiansIndexes]) => {
             const node = localParties[nodeIndex as number - 1];
             const guardians = (guardiansIndexes as number[]).map((index) => localParties[index - 1].publicParty);
 
-            const { encryptedShares } = node.createSharesWithoutProofs(guardians)
+            const { encryptedShares, shares } = node.createSharesWithoutProofs(guardians)
+            assert.equal(recoverZ(_.sampleSize(shares, config.guardiansThreshold), config.guardiansSize, config.guardiansThreshold), node.votingKeypair.privKey)
             votingPublicKeys.push(node.publicParty.votingPublicKey)
             sharesFrom.set(node.publicParty.publicKey, encryptedShares)
         })
@@ -58,8 +62,7 @@ describe("fdkg", () => {
         // voting
         nodeIndicies.map((nodeIndex) => {
             const node = localParties[nodeIndex - 1]
-            const r = genRandomSalt()
-            const { C1, C2, cast } = node.prepareBallot(votingPublicKey, r)
+            const { C1, C2, cast } = node.prepareBallot(votingPublicKey)
             casts[cast - 1] += 1n
             votes.push({ C1, C2 })
         })
@@ -67,27 +70,48 @@ describe("fdkg", () => {
         // tally
         const C1 = votes.map(v => v.C1).reduce(addPoint, PointZero)
 
-        nodeIndicies.forEach((nodeIndex) => {
+        talliers.forEach((nodeIndex) => {
             const node = localParties[nodeIndex - 1]
-
-            const sharesForNode: Array<EncryptedShare & { index: number, sharesSize: number }> = []
+            if (!node) {
+                throw new Error(`Node ${nodeIndex - 1} not found in ${localParties}`)
+            }
 
             for (let [from, tos] of sharesFrom) {
                 const index = tos.findIndex(to => to.guardianPubKey === node.publicParty.publicKey)
                 if (index != -1) {
-                    sharesForNode.push({ index: index + 1, sharesSize: tos.length, ...tos[index] })
+                    console.log(`node ${nodeIndex} found share from ${localParties.findIndex(p => p.publicParty.publicKey == from) + 1}`)
+                    const share = tos[index]
+                    const partialDecryption = node.partialDecryptionForEncryptedShare(C1, share)
+
+                    partialDecryptionFor.has(from) ?
+                        partialDecryptionFor.get(from)?.push({ senderPubKey: node.publicParty.publicKey, pd: partialDecryption }) :
+                        partialDecryptionFor.set(from, [{ senderPubKey: node.publicParty.publicKey, pd: partialDecryption }])
                 }
             }
-
-            console.log({ tallerIndex: nodeIndex, shares: sharesForNode.length })
-            // console.log(`Taller ${nodeIndex} received ${sharesForNode.length} shares`)
-            sharesForNode.forEach(share => {
-                const partialDecryption = node.partialDecryptionForEncryptedShare(C1, share)
-                partialDecryptions.push(partialDecryption)
-            })
-
         })
 
+
+        for (let [from, pds] of partialDecryptionFor) {
+            // nodes of participants
+            const nodes: { pd: BabyJubPoint, shareIndex: number }[] = []
+            for (let { senderPubKey, pd } of pds) {
+                const shareIndex = sharesFrom.get(from)?.findIndex(share => share.guardianPubKey === senderPubKey)
+                if (shareIndex == undefined) {
+                    throw new Error("Partial decryption for share of party that did not contribute to dkg")
+                }
+                if (shareIndex == -1) {
+                    throw new Error("Partial decryption from node that didn't received the share")
+                }
+                nodes.push({ pd, shareIndex: shareIndex + 1 })
+            }
+
+            const partialDecryption = nodes.reduce((acc, { pd, shareIndex }) => {
+                const lagrangeCoeff = LagrangeCoefficient(shareIndex, nodes.map(n => n.shareIndex ))
+                return addPoint(acc, mulPointEscalar(pd, lagrangeCoeff))
+            }, PointZero)
+            partialDecryptions.push(partialDecryption)
+        }
+        // offline tally
         const Z = partialDecryptions.reduce(addPoint, PointZero)
         assert(inCurve(Z))
 
@@ -98,7 +122,7 @@ describe("fdkg", () => {
             const decryptedCasts = decryptResults(Z, C2, config.size, config.options)
             assert.deepEqual(decryptedCasts, casts)
         } catch (e) {
-            console.error("😩 Could not decrypt results")
+            assert(!e, "😩 Could not decrypt results")
         }
     })
 });
--- shared-crypto/src/fdkg.ts
+++ shared-crypto/src/fdkg.ts
@@ -3,25 +3,29 @@ import * as F_Base8 from "./FBase8";
 import { LagrangeCoefficient } from "./sss";
 import { mulPointEscalar } from "./babyjub";
 
-type Share = {
+export type Share = {
     x: number, // x coordinate of the share
     y: bigint, // share value which is y = f(x)
-    nodes: number[] // X coordinates of other published shares
 }
-export const shareWithLagrange = (share: Share): bigint => {
+
+export type CollectedShare = Share & {
+    nodes: number[] // X coordinates of the collected nodes
+}
+
+export const shareWithLagrange = (share: CollectedShare): bigint => {
     const lagrangeBasis = LagrangeCoefficient(share.x, share.nodes)
     const lagrangeWithShare = F_Base8.mul(lagrangeBasis, share.y)
     return lagrangeWithShare
 }
 
-const aggregateSharesWithLagrange = (shares: Share[]): bigint => {
+const aggregateSharesWithLagrange = (shares: CollectedShare[]): bigint => {
     return shares.reduce((acc, share) => {
         const lagrangeWithShare = shareWithLagrange(share)
         return F_Base8.add(acc, lagrangeWithShare)
     }, F_Base8.zero)
 }
 
-export const partialDecryption = (C1: BabyJubPoint, shares: Share[]): BabyJubPoint => {
+export const partialDecryption = (C1: BabyJubPoint, shares: CollectedShare[]): BabyJubPoint => {
     const dkgVotingPrivKeyShare = aggregateSharesWithLagrange(shares)
     return mulPointEscalar(C1, dkgVotingPrivKeyShare)
 }
