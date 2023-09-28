ae3e69eca5ca2417955d9f83c90b1eae089756d5
Stanislaw Baranski <stan@stan.bar>
2023-09-28 17:03:11 +0200

    update setup scripts

--- circuits/setup.sh
+++ circuits/setup.sh
@@ -1,5 +1,11 @@
+npx circomkit compile pvss_1_of_2
+snarkjs plonk setup build/pvss_1_of_2/pvss_1_of_2.r1cs ptau/powersOfTau28_hez_final_17.ptau plonk_pkey.zkey
+mv plonk_pkey.zkey build/pvss_1_of_2/
+snarkjs zkey export verificationkey build/pvss_1_of_2/plonk_pkey.zkey build/pvss_1_of_2/plonk_vkey.json
+
 npx circomkit compile pvss_2_of_3
-snarkjs plonk setup build/pvss_2_of_3/pvss_2_of_3.r1cs ptau/powersOfTau28_hez_final_17.ptau build/pvss_2_of_3/plonk_pkey.zkey
+snarkjs plonk setup build/pvss_2_of_3/pvss_2_of_3.r1cs ptau/powersOfTau28_hez_final_17.ptau plonk_pkey.zkey
+mv plonk_pkey.zkey build/pvss_2_of_3/
 snarkjs zkey export verificationkey build/pvss_2_of_3/plonk_pkey.zkey build/pvss_2_of_3/plonk_vkey.json
 
 npx circomkit compile pvss_3_of_4
--- circuits/src/setup.ts
+++ circuits/src/setup.ts
@@ -3,13 +3,13 @@ import { Circomkit } from "circomkit";
 export const circomkit = new Circomkit({
     verbose: false,
     logLevel: "error",
-    protocol: "groth16"
+    protocol: "plonk"
 });
 // import circuits from "../circuits.json" assert { type: "json" };
 const circuits = [
     // { circuit: "pvss_3_of_4", ptau: "./ptau/powersOfTau28_hez_final_18.ptau" },
-    { circuit: "pvss_2_of_3", ptau: "./ptau/powersOfTau28_hez_final_17.ptau" },
-    // { circuit: "pvss_1_of_2", ptau: "./ptau/powersOfTau28_hez_final_16.ptau" },
+    // { circuit: "pvss_2_of_3", ptau: "./ptau/powersOfTau28_hez_final_17.ptau" },
+    { circuit: "pvss_1_of_2", ptau: "./ptau/powersOfTau28_hez_final_16.ptau" },
     // { circuit: "partial_decryption", ptau: "./ptau/powersOfTau28_hez_final_14.ptau" },
     // { circuit: "decrypt_share", ptau: "./ptau/powersOfTau28_hez_final_13.ptau" },
     // { circuit: "encrypt_ballot", ptau: "./ptau/powersOfTau28_hez_final_15.ptau" }
