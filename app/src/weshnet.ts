d8827cb8be867eca6b13eb90da058caec8ecec13
Stanislaw Baranski <stan@stan.bar>
2023-10-09 17:57:30 +0200

    remove ffi and use stdio communication

--- app/src/weshnet.ts
+++ app/src/weshnet.ts
@@ -1,27 +1,59 @@
 import sizeof from 'object-sizeof'
 import { EncryptedShare, PublicParty } from './party'
 import { BabyJubPoint, Proof, PubKey, PublicSignals } from 'shared-crypto'
+import { spawn } from 'child_process';
 
-import ffi from 'ffi-napi'
-import ref from 'ref-napi'
+const weshnetBin = process.env.WESHNET_BIN ? process.env.WESHNET_BIN : '../../weshnet/bin/weshnet'
 
-// Define the types and functions from the shared library
-const libweshnet = ffi.Library('libweshnet.so.dylib', {
-  NewPersistentServiceClient: ['void', ['string']] // replace with actual function signature
-  // Add other functions as needed
-})
+export class MyGoApp {
+  private process;
 
-if (process.env.WESHNET) {
-  // Call the function
-  libweshnet.NewPersistentServiceClient('data1')
+  constructor() {
+    // Initialize and start the Go binary as a subprocess
+    this.process = spawn(weshnetBin);
+
+    // Listen to the subprocess stdout and log it
+    this.process.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    // Listen to the subprocess stderr and log it
+    this.process.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    // Log when the subprocess closes
+    this.process.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  }
+
+  // Function to send a command to the Go app
+  sendCommand(command: string) {
+    this.process.stdin.write(command + '\n');
+  }
+
+  // Function to stop the Go app
+  stop() {
+    this.process.kill();
+  }
+}
+
+const app = new MyGoApp();
+
+export const init = (node: PublicParty, id: string | number) => {
+  app.sendCommand(`init ${node.index}`);
 }
 
 export const broadcastContributeDkg = async (node: PublicParty, encryptedShares: EncryptedShare[], votingPublicKey: PubKey, proof?: Proof, publicSignals?: PublicSignals) => {
   console.log({ encryptedShares: sizeof({ encryptedShares, votingPublicKey, proof, publicSignals }) })
+  app.sendCommand(`sendMessage ${JSON.stringify({ contributeDkg: encryptedShares })}`)
 }
 
 export const broadcastPublishVote = async (node: PublicParty, encryptedBallot: { C1: BabyJubPoint, C2: BabyJubPoint, proof?: Proof, publicSignals?: PublicSignals }) => {
   console.log({ encryptedBallot: sizeof(encryptedBallot) })
+
+  app.sendCommand(`sendMessage ${JSON.stringify({ publishVote: encryptedBallot })}`)
 }
 
 export const broadcastPublishPartialDecryption = async (node: PublicParty, partialDecryption: Array<{
@@ -31,4 +63,6 @@ export const broadcastPublishPartialDecryption = async (node: PublicParty, parti
   publicSignals?: PublicSignals
 }>) => {
   console.log({ partialDecryption: sizeof(partialDecryption) })
+
+  app.sendCommand(`sendMessage ${JSON.stringify({ partialDecryption: partialDecryption })}`)
 }
