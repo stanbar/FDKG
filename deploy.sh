5f58f9eec65fecdb8a1c4437c6c94224329ab2a8
Stanislaw Baranski <stan@stan.bar>
2024-12-07 14:08:54 +0100

    update deplo

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-yarn workspace app build && \
+yarn workspace app run build && \
 echo "Updating gh-pages branch" && \
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && \
 echo "Pushing to github" && \
