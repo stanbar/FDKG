08d6f5de1729497591243a3a64422b1aefe7188a
Stanislaw Baranski <stan@stan.bar>
2025-01-28 14:36:55 +0100

    fix step by

--- liveness_sim/src/main.rs
+++ liveness_sim/src/main.rs
@@ -93,7 +93,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         } else {
             for guardians in (2..=nodes - 1).step_by(std::cmp::max(10, (nodes - 1) / 10)) {
-                for threshold in (1..=guardians).step_by(std::cmp::min(1, guardians / 10)) {
+                for threshold in (1..=guardians).step_by(std::cmp::max(1, guardians / 10)) {
                     for &fdkg_pct in &fdkg_percentages {
                         for &tallier_ret_pct in &tallier_returning_percentages {
                             for &tallier_new_pct in &tallier_new_percentages {
